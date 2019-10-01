556a22367fdd0e5e1eb48238aaf1918cce79f548
rustam.gainutdinov <rustamikc@gmail.com>
2019-10-01 19:17:48 +0300

    change config

--- bin/config/appConfig.js
+++ bin/config/appConfig.js
@@ -4,10 +4,10 @@ const APP_CONFIG = {
     listenPort: 8001,
     appName: 'Example Application',
     db: {
-        user: process.env.NODE_ENV === 'production' ? '' : 'postgres',
+        user: process.env.NODE_ENV === 'production' ? 'postgres' : 'postgres',
         host: 'localhost',
-        database: process.env.NODE_ENV === 'production' ? '' : 'speed-test-service',
-        password: process.env.NODE_ENV === 'production' ? '' : '!QA2ws#ED',
+        database: process.env.NODE_ENV === 'production' ? 'speed_test_service' : 'speed-test-service',
+        password: process.env.NODE_ENV === 'production' ? 'Ehwuidzn0672' : '!QA2ws#ED',
         port: 5432
     }
 };
