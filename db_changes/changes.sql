995368537d6a0bab187ccb18ed958bb79cd7b45d
rustam.gainutdinov <rustam.gainutdinov@ispring.ru>
2019-10-07 10:42:46 +0300

    добавлены скрипты для инициализации db

--- db_changes/changes.sql
+++ db_changes/changes.sql
@@ -1,5 +1,5 @@
-alter table domain
+alter table public.domain
 	add display_priority int;
 
 create unique index domain_display_priority_uindex
-	on domain (display_priority);
+	on public.domain (display_priority);
