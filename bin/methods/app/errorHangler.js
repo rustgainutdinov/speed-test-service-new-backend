6a346136416292c28e0af0e0f494c5c65ec89c05
rustam.gainutdinov <rustam.gainutdinov@ispring.ru>
2019-10-07 10:54:41 +0300

    изменен обработчик ошибок

--- bin/methods/app/errorHangler.js
+++ bin/methods/app/errorHangler.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 function errorHandler(err, req, res, next) {
-    res.status(500).json(err.message);
+    res.status(404).json(err.message);
 }
 exports.default = errorHandler;
--- src/methods/app/errorHangler.ts
+++ src/methods/app/errorHangler.ts
@@ -1,7 +1,7 @@
 import {NextFunction, Request, Response} from "express";
 
 function errorHandler(err: Error, req: Request, res: Response, next: NextFunction) {
-    res.status(500).json(err.message);
+    res.status(404).json(err.message);
 }
 
 export default errorHandler
